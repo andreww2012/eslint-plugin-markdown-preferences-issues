2791cf5a2faf8a0a31ced8401d426e3be8841a0f
Andrew Kazakov <andreww_dev@proton.me>
2025-10-05 22:59:37 +0300

    feat: add heading-casing-suggests-replacing-i-in-sentence-case issue

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,28 @@ export default defineConfig([
     },
     language: 'markdown-preferences/extended-syntax',
     files: ['**/*.md'],
+  },
+  {
+    files: ['src/issues/prefer-link-reference-definitions-autofix-removes-images.md'],
     rules: {
       'markdown-preferences/prefer-link-reference-definitions': 2,
+    },
+  },
+  {
+    files: ['src/issues/heading-casing-suggests-replacing-i-in-sentence-case.md'],
+    rules: {
+      'markdown-preferences/heading-casing': [
+        2,
+        {
+          style: 'Sentence case',
+        },
+      ],
+      'markdown-preferences/table-header-casing': [
+        2,
+        {
+          style: 'Sentence case',
+        },
+      ],
     },
   },
 ]);
